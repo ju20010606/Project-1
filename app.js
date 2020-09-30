f3c60597357ccf61c36e410001c088bb70434520
Juliana Fernandes <julianafernandes@Albertos-MacBook-Pro.local>
2020-09-29 23:05:20 -0300

    first draft of pseudocode of game (javascript file)

---
+++ app.js
@@ -0,0 +1,6 @@
+//array of images
+// randomize between images 
+//give random image to 2 divs if clicked
+//if two divs clicked have the same image than keep them turned to the images side 
+//if two divs don't have the same image turn them back
+//if all divs have images = win!
