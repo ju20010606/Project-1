6c3ac3d309d2f93a2a78b4459367f3b12967550e
Juliana Fernandes <julianafernandes@Albertos-MacBook-Pro.local>
2020-10-02 14:58:20 -0300

    added condition for similar divs to go back to default flower image

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let imgOfRose = "https://img.elo7.com.br/product/original/1DCCA20/lirio-permanente-buque-c-5-lirios-flor-artificial-azul-azul.jpg"
+
+
 //array of images
 let img1 =
  "https://i.insider.com/5dcae88e79d75759bc2e4e15?width=750&format=jpeg&auto=webp";
@@ -57,19 +60,21 @@ randomImgs = () => {
       squares[i].addEventListener("click",()=>{
         squares[i].innerHTML = `<img src=${randImg} /img>`;
         if(squares[i].innerHTML === squares[j].innerHTML){
-            console.log("it works")
-        }
+            setTimeout(()=>{
+                squares[i].innerHTML = `<img src=${imgOfRose} /img>`
+                squares[j].innerHTML = `<img src=${imgOfRose} /img>`
+            },500)
+        } 
       })
       
       squares[j].addEventListener("click",()=>{
         squares[j].innerHTML = `<img src=${randImg} /img>`;
-        if(squares[i].innerHTML === squares[j].innerHTML){
-            console.log("it works")
-        }
       })
       
     }
   };
+
+
 
 restart = () => {
   document.querySelector(".newGame").addEventListener("click", () => {
@@ -78,9 +83,6 @@ restart = () => {
   });
 };
 
-win = () =>{
-    
-}
 
 
 
